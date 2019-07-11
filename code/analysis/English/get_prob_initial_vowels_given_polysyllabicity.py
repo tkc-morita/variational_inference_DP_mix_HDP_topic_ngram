0ab94dbcae25c340c0d093c5557fee875e6ecce0
Takashi Morita <takashi@h106.177.225.10.102937.vlan.kuins.net>
2019-07-12 07:48:20 +0900

    Implemented computation of prob. of init vowels given polysyllabicity.

--- code/analysis/English/get_prob_initial_vowels_given_polysyllabicity.py
+++ code/analysis/English/get_prob_initial_vowels_given_polysyllabicity.py
@@ -14,19 +14,21 @@ import get_prob_substrings_occur_in_prefixes, encode_decode, get_substring_repre
 
 
 def mask_VX_transitions(ngram_array, coded_vowels):
-	for v in coded_vowels:
-		ngram_array[...,v:,:,:] = 0.0
-	return ngram_array
+	mask = np.zeros_like(ngram_array, dtype=int)
+	# for v in coded_vowels:
+	mask[...,coded_vowels,:,:] = 1
+	return np.ma.masked_array(ngram_array, mask=mask)
 
 VOWELS_IN_DISC = list('IE{VQU@i#$u312456789cq0~')
 # CONSONANTS_IN_DISC = list('pbtdkgNmnlrfvTDszSZjxhwJ_CFHPR')
 
-def mask_last_C(ngram_array, coded_consonants):
-	for c in coded_consonants:
-		ngram_array[...,c,:] = 0.0
-	return ngram_array
+def mask_last_non_V(ngram_array, coded_vowels):
+	mask = np.ones_like(ngram_array, dtype=int)
+	# for c in coded_vowels:
+	mask[...,coded_vowels,:] = 0
+	return np.ma.masked_array(ngram_array, mask=mask)
 
-def get_prob_init_V_given_polysyllabicity(ngram_array, max_length_init_cc, max_length_internal_cc, coded_vowels, coded_consonants):
+def get_prob_init_V_given_polysyllabicity(ngram_array, max_length_init_cc, max_length_internal_cc, coded_vowels):
 	"""
 	Get the probability of word-initial vowel (V1) given sublex and presence of another vowel (V2) in the word.
 	V1 is maximally preceded by max_length_init_cc consonants, and V1 and V2 are intervened by 0 to max_length_internal_cc consonants.
@@ -44,97 +46,78 @@ def get_prob_init_V_given_polysyllabicity(ngram_array, max_length_init_cc, max_l
 		sub_cum = sub_cum[start_code]
 	sub_cum[:] = sub_array # p(x | start*(n-1))
 
-	prob_absent = 1.0 - prob_after_k_trans
-
 	ngram_array_VX_masked = mask_VX_transitions(ngram_array, coded_vowels)
 
-	prob_init_V_given_polysyllabicity = hoge(mask_last_C(prob_after_k_trans.copy()).sum(axis=0), ngram_array, max_length_internal_cc, ngram_array_VX_masked=ngram_array_VX_masked, coded_vowels=coded_vowels)
+	prob_init_V_given_polysyllabicity = get_prob_V_before_CstarV(mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=0), ngram_array, max_length_internal_cc, coded_vowels, ngram_array_VX_masked=ngram_array_VX_masked)
 
-	for k in range(max_length_init_cc+1):
-		marg_over_oldest_trans = np.sum(prob_after_k_trans,axis=0)
+	for k in range(1, max_length_init_cc+1):
 		prob_after_k_trans = (
-						marg_over_oldest_trans.reshape(
-							marg_over_oldest_trans.shape[:-1]
-							+ (1,marg_over_oldest_trans.shape[-1])
-						) # shape=(vocab_size)^(n-1) x 1 x num_sublex
+						np.sum(prob_after_k_trans,axis=0)[...,np.newaxis,:] # shape=(vocab_size)^(n-1) x 1 x num_sublex
 						*
 						ngram_array_VX_masked # shape=(vocab_size)^n x num_sublex
 						)
-		prob_init_V_given_polysyllabicity += hoge(mask_last_C(prob_after_k_trans.copy()).sum(axis=0), ngram_array, max_length_internal_cc, ngram_array_VX_masked=ngram_array_VX_masked, coded_vowels=coded_vowels)
-	return prob_init_V_given_polysyllabicity # i.e. prob. of existance.
+		prob_init_V_given_polysyllabicity += get_prob_V_before_CstarV(mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=0), ngram_array, max_length_internal_cc, coded_vowels, ngram_array_VX_masked=ngram_array_VX_masked)
+	return prob_init_V_given_polysyllabicity.filled(fill_value=0.0)
 
-def hoge(init_array, ngram_array, max_length_cc, ngram_array_VX_masked=None, coded_vowels=None):
+def get_prob_V_before_CstarV(init_context_w_last_c_masked, ngram_array, max_length_cc, coded_vowels, ngram_array_VX_masked=None):
 	if ngram_array_VX_masked is None:
-		if coded_vowels is None:
-			raise ValueError('Either ngram_array_VX_masked or coded_vowels needs to be specified.')
-		else:
-			ngram_array_VX_masked = mask_VX_transitions(ngram_array, coded_vowels)
+		ngram_array_VX_masked = mask_VX_transitions(ngram_array, coded_vowels)
+	# The only V-to-X transision.
 	prob_after_k_trans = (
-						init_array.reshape(
-							init_array.shape[:-1]
-							+ (1,init_array.shape[-1])
-						) # shape=(vocab_size)^(n-1) x 1 x num_sublex
+						init_context_w_last_c_masked[...,np.newaxis,:] # shape=(vocab_size)^(n-1) x 1 x num_sublex
 						*
 						ngram_array # shape=(vocab_size)^n x num_sublex
 						)
-	prefix_length = init_array.ndim-2
+	prefix_length = init_context_w_last_c_masked.ndim-2
+	prob_init_V_given_polysyllabicity = mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=tuple(range(prefix_length))+(-2,))
+	prefix_reminder = prefix_length
 	if max_length_cc < prefix_length:
-		for k in range(1,max_length_cc):
+		for k in range(1,max_length_cc+1):
 			prob_after_k_trans = (
-						marg_over_oldest_trans.reshape(
-							marg_over_oldest_trans.shape[:-1]
-							+ (1,marg_over_oldest_trans.shape[-1])
-						) # shape=(vocab_size)^(n-1) x 1 x num_sublex
+						np.sum(prob_after_k_trans,axis=0)[...,np.newaxis,:] # shape=(vocab_size)^(n-1) x 1 x num_sublex
 						*
 						ngram_array_VX_masked # shape=(vocab_size)^n x num_sublex
 						)
-			marg_over_oldest_trans = np.sum(prob_after_k_trans,axis=0)
-		prob_after_k_trans = (
-								marg_over_oldest_trans.reshape(
-									marg_over_oldest_trans.shape[:-1]
-									+ (1,marg_over_oldest_trans.shape[-1])
-								) # shape=(vocab_size)^(n-1) x 1 x num_sublex
-								*
-								ngram_array # shape=(vocab_size)^n x num_sublex
-								)
-		# marg_over_prefix = prob_after_k_trans.sum(axis=tuple(range(prefix_length - max_length_cc)))
+			prefix_reminder -= 1
+			prob_init_V_given_polysyllabicity += mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=
+										tuple(range(prefix_reminder))
+										+
+										tuple(range(-k-2,-1)) # -2: second V, -1: sublex
+										)
 	else:
 		for k in range(1, prefix_length):
 			prob_after_k_trans = (
-							marg_over_oldest_trans.reshape(
-								marg_over_oldest_trans.shape[:-1]
-								+ (1,marg_over_oldest_trans.shape[-1])
-							) # shape=(vocab_size)^(n-1) x 1 x num_sublex
+							np.sum(prob_after_k_trans,axis=0)[...,np.newaxis,:] # shape=(vocab_size)^(n-1) x 1 x num_sublex
 							*
 							ngram_array_VX_masked # shape=(vocab_size)^n x num_sublex
 							)
-			marg_over_oldest_trans = np.sum(prob_after_k_trans,axis=0)
-		for k in range(prefix_length, max_length_cc):
+			prefix_reminder -= 1
+			prob_init_V_given_polysyllabicity += mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=
+										tuple(range(prefix_reminder))
+										+
+										tuple(range(-k-2,-1)) # -2: second V, -1: sublex
+										)
+		prob_after_k_trans = (
+							np.sum(prob_after_k_trans,axis=0)[...,np.newaxis,:] # shape=(vocab_size)^(n-1) x 1 x num_sublex
+							*
+							ngram_array_VX_masked # shape= 1 x (vocab_size)^n x num_sublex
+							) # The 0th axis is the first Vs now.
+		prob_init_V_given_polysyllabicity += mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=tuple(range(1,prob_after_k_trans.ndim-1)))
+		for k in range(prefix_length+1, max_length_cc+1):
 			prob_after_k_trans = (
-							prob_after_k_trans.reshape(
-								prob_after_k_trans.shape[:-1]
-								+ (1,prob_after_k_trans.shape[-1])
-							) # shape=(vocab_size)^n x 1 x num_sublex
+							prob_after_k_trans[...,np.newaxis,:] # shape=(vocab_size)^n x 1 x num_sublex
 							*
 							ngram_array_VX_masked.reshape((1,)+ngram_array_VX_masked.shape)# shape= 1 x (vocab_size)^n x num_sublex
-							)
-			marg_over_oldest_trans = np.sum(prob_after_k_trans,axis=1) # Skip the target axis.
-		prob_after_k_trans = (
-							prob_after_k_trans.reshape(
-								prob_after_k_trans.shape[:-1]
-								+ (1,prob_after_k_trans.shape[-1])
-							) # shape=(vocab_size)^n x 1 x num_sublex
-							*
-							ngram_array.reshape((1,)+ngram_array.shape)# shape= 1 x (vocab_size)^n x num_sublex
-							)
-		return prob_after_k_trans.sum(axis=tuple(range(1,prob_after_k_trans.ndim-1)))
-		
+							).sum(axis=1)
+			prob_init_V_given_polysyllabicity += mask_last_non_V(prob_after_k_trans, coded_vowels).sum(axis=tuple(range(1,prob_after_k_trans.ndim-1)))
+	return prob_init_V_given_polysyllabicity
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('ngram_path', type=str, help='Path to the csv file containing ngram prob. info.')
 	parser.add_argument('code_path', type=str, help='Path to the csv file containing segment coding.')
-	parser.add_argument('max_CC_length', type=int, help='Maximum length of the initial consonant cluster before the first vowel.')
+	parser.add_argument('max_length_init_cc', type=int, help='Maximum length of the initial consonant cluster before the first vowel.')
+	parser.add_argument('max_length_internal_cc', type=int, help='Maximum length of the consonant cluster between the first and second vowels.')
 	parser.add_argument('save_path', type=str, help='Path to the csv file to save results.')
 	args = parser.parse_args()
 
@@ -145,23 +128,10 @@ if __name__ == "__main__":
 	df_code = pd.read_csv(args.code_path, encoding='utf-8')
 	encoder, decoder = encode_decode.df2coder(df_code)
 	coded_vowels = [encoder[v] for v in VOWELS_IN_DISC]
-	# coded_consonants = [encoder[c] for c in CONSONANTS_IN_DISC]
-
-	ngram_array = mask_VX_transitions(ngram_array, coded_vowels)
 
 
-	joint_probs_of_n_grams = get_prob_substrings_occur_in_prefixes.get_prob_substring_occur_in_prefixes(ngram_array, args.max_CC_length+1)
+	prob_init_V_given_polysyllabicity = get_prob_init_V_given_polysyllabicity(ngram_array, args.max_length_init_cc, args.max_length_internal_cc, coded_vowels)
 
-	
-	
-	probs_last_symbol = np.sum(
-							joint_probs_of_n_grams.reshape(
-								-1,
-								joint_probs_of_n_grams.shape[-2],
-								joint_probs_of_n_grams.shape[-1]
-								),
-							axis=0
-							)
 
 	log_assignment_probs = parse_vi.get_log_sublex_assignment_probs(os.path.dirname(args.code_path))
 	log_assignment_over_others = get_substring_representativeness.get_log_assignment_over_others(log_assignment_probs)
@@ -169,7 +139,7 @@ if __name__ == "__main__":
 
 	results = []
 	for v in VOWELS_IN_DISC:
-		probs_v = probs_last_symbol[encoder[v],:]
+		probs_v = prob_init_V_given_polysyllabicity[encoder[v],:]
 		representativeness = get_substring_representativeness.get_representativeness(np.log(probs_v), log_assignment_over_others)
 		results += [(v,sublex_ix,prob,rep) for sublex_ix,(prob,rep) in enumerate(zip(probs_v,representativeness))]
 	df_result = pd.DataFrame(results, columns=['vowel','sublex','prob','representativeness'])
